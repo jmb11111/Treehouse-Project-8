370cebc756872e3f29fa0017fc8dd8279e8374ce
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-07-19 17:36:25 -0400

    server working

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,72 @@
 const gulp = require("gulp"),
     concat = require("gulp-concat"),
     rename = require("gulp-rename"),
-    sass = require("gulp-sass"),
-    map = require("gulp-sourcemap"),
+      sass = require("gulp-sass"),
+       map = require("gulp-sourcemaps"),
+ cssMinify = require("gulp-clean-css"),
+       del = require("del"),
+ webserver = require('gulp-webserver'),
     minify = require("gulp-uglify");
 
 gulp.task("concatScripts", ()=>{
-   return gulp.src(["js/circle/**.js"])
+   return gulp.src(["jquery.js,scripts/**.js","scripts/circle/**.js"])
+    .pipe(map.init())
     .pipe(concat("global.js"))
-    .pipe(gulp.dest('js'));
+    .pipe(map.write("./"))
+    .pipe(gulp.dest('scripts'));
 });
 
 gulp.task("minifyScrips",["concatScripts"], ()=>{
-   return gulp.src("js/global.js")
+   return gulp.src("scripts/global.js")
     .pipe(minify())
     .pipe(rename('all.min.js'))
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('scripts'));
 });
 
 gulp.task("compileSass", ()=>{
     return gulp.src("sass/global.scss")
+    .pipe(map.init())
     .pipe(sass())
-    .pipe(gulp.dest("css"));
+    .pipe(map.write("./"))
+    .pipe(gulp.dest("styles"));
 })
 gulp.task("minifySass",["compileSass"], ()=>{
-    return gulp.src("css/global.css")
-    .pipe(minify())
+    return gulp.src("styles/global.css")
+    .pipe(cssMinify())
     .pipe(rename("all.min.css"))
-    .pipe(gulp.dest("css"));
+    .pipe(gulp.dest("styles"));
 
 })
 
 gulp.task("scripts",["minifyScrips"])
+
+gulp.task("styles",["minifySass"])
+
+gulp.task("clean",()=>{
+del(["dist", "styles", "scripts/**.js*"]);
+})
+
+gulp.task("distribution",["clean","scripts","styles"], ()=>{
+    return gulp.src(["scripts/all.min.js", "styles/all.min.css",
+    "index.html","images/**", "icons/**" ],{base: './'})
+    .pipe(gulp.dest('dist'));
+
+})
+
+gulp.task("build",["distribution"])
+
+gulp.task("startServer",['build'],()=>{
+    gulp.src('dist')
+    .pipe(webserver({
+        port: 3000,
+        livereload: true,
+        open: true,
+    }));
+
+
+})
+
+gulp.task("default",["build"], ()=>{
+   
+
+})
