8c49b9936dc977679e8d84a9a5e7168b0c847381
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-07-19 15:03:28 -0400

    gulp installed, concat scrips working

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    glob = require("glob"),
+    minify = require("gulp-uglify");
+
+gulp.task("concatScripts", ()=>{
+    gulp.src(["js/circle/**.js"])
+    .pipe(concat("global.js"))
+    .pipe(gulp.dest('js'));
+
+
+});
