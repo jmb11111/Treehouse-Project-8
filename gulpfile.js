6fd93295f080461aece310eb37ff017040f16efe
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-07-19 15:51:16 -0400

    gulp js done, working on scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,35 @@
 
 const gulp = require("gulp"),
     concat = require("gulp-concat"),
-    glob = require("glob"),
+    rename = require("gulp-rename"),
+    sass = require("gulp-sass"),
+    map = require("gulp-sourcemap"),
     minify = require("gulp-uglify");
 
 gulp.task("concatScripts", ()=>{
-    gulp.src(["js/circle/**.js"])
+   return gulp.src(["js/circle/**.js"])
     .pipe(concat("global.js"))
     .pipe(gulp.dest('js'));
 });
 
-gulp.task("minifyScrips", ()=>{
-    gulp.src("js/global.js")
+gulp.task("minifyScrips",["concatScripts"], ()=>{
+   return gulp.src("js/global.js")
     .pipe(minify())
+    .pipe(rename('all.min.js'))
     .pipe(gulp.dest('js'));
 });
+
+gulp.task("compileSass", ()=>{
+    return gulp.src("sass/global.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("css"));
+})
+gulp.task("minifySass",["compileSass"], ()=>{
+    return gulp.src("css/global.css")
+    .pipe(minify())
+    .pipe(rename("all.min.css"))
+    .pipe(gulp.dest("css"));
+
+})
+
+gulp.task("scripts",["minifyScrips"])
--- js/global.js
+++ js/global.js
@@ -1,1 +1,75 @@
-!function(c){c.fn.autogrow=function(s){function a(e){var t,i=c(this),n=i.innerHeight(),a=this.scrollHeight,l=i.data("autogrow-start-height")||0;if(n<a)this.scrollTop=0,s.animate?i.stop().animate({height:a},s.speed):i.innerHeight(a);else if(!e||8==e.which||46==e.which||e.ctrlKey&&88==e.which)if(l<n){for(t=i.clone().addClass(s.cloneClass).css({position:"absolute",zIndex:-10,height:""}).val(i.val()),i.after(t);a=t[0].scrollHeight-1,t.innerHeight(a),a===t[0].scrollHeight;);a++,t.remove(),i.focus(),a<l&&(a=l),a<n&&s.animate?i.stop().animate({height:a},s.speed):i.innerHeight(a)}else i.innerHeight(l)}var e=c(this).css({overflow:"hidden",resize:"none"}),t=e.selector,i={context:c(document),animate:!0,speed:200,fixMinHeight:!0,cloneClass:"autogrowclone",onInitialize:!1};return s=c.isPlainObject(s)?s:{context:s||c(document)},s=c.extend({},i,s),e.each(function(e,t){var i,n;(t=c(t)).is(":visible")||0<parseInt(t.css("height"),10)?i=parseInt(t.css("height"),10)||t.innerHeight():(n=t.clone().addClass(s.cloneClass).val(t.val()).css({position:"absolute",visibility:"hidden",display:"block"}),c("body").append(n),i=n.innerHeight(),n.remove()),s.fixMinHeight&&t.data("autogrow-start-height",i),t.css("height",i),s.onInitialize&&t.length&&a.call(t[0])}),s.context.on("keyup paste",t,a),e}}(jQuery),$(document).ready(function(){$("textarea").autogrow({onInitialize:!0}),$(".circle--clone--list").on("click",".circle--clone--add",function(){var e=$(this).parent("li"),t=e.clone();e.after(t),t.find("input, textarea, select").val(""),t.find("*:first-child").focus()}),$(".circle--clone--list").on("click","li:not(:only-child) .circle--clone--remove",function(){$(this).parent("li").remove()}),$(".circle--pill--list a").click(function(){$(".circle--pill--list a").removeClass("selected"),$(this).addClass("selected")}),$(".circle--select select").focus(function(){$(this).parent().addClass("focus")}).blur(function(){$(this).parent().removeClass("focus")}),$(".clickable-row").click(function(){var e=$(this).data("href"),t=$(this).data("target");$(this).attr("data-target")?window.open(e,t):window.open(e,"_self")});var e=$(".circle--input--file"),t=e.data("text"),i=e.data("state");e.wrap(function(){return"<a class='button "+i+"'>"+t+"</div>"})});
+/*
+autogrow.js - Copyright (C) 2014, Jason Edelman <edelman.jason@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is furnished to
+do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+;(function(e){e.fn.autogrow=function(t){function s(n){var r=e(this),i=r.innerHeight(),s=this.scrollHeight,o=r.data("autogrow-start-height")||0,u;if(i<s){this.scrollTop=0;t.animate?r.stop().animate({height:s},t.speed):r.innerHeight(s)}else if(!n||n.which==8||n.which==46||n.ctrlKey&&n.which==88){if(i>o){u=r.clone().addClass(t.cloneClass).css({position:"absolute",zIndex:-10,height:""}).val(r.val());r.after(u);do{s=u[0].scrollHeight-1;u.innerHeight(s)}while(s===u[0].scrollHeight);s++;u.remove();r.focus();s<o&&(s=o);i>s&&t.animate?r.stop().animate({height:s},t.speed):r.innerHeight(s)}else{r.innerHeight(o)}}}var n=e(this).css({overflow:"hidden",resize:"none"}),r=n.selector,i={context:e(document),animate:true,speed:200,fixMinHeight:true,cloneClass:"autogrowclone",onInitialize:false};t=e.isPlainObject(t)?t:{context:t?t:e(document)};t=e.extend({},i,t);n.each(function(n,r){var i,o;r=e(r);if(r.is(":visible")||parseInt(r.css("height"),10)>0){i=parseInt(r.css("height"),10)||r.innerHeight()}else{o=r.clone().addClass(t.cloneClass).val(r.val()).css({position:"absolute",visibility:"hidden",display:"block"});e("body").append(o);i=o.innerHeight();o.remove()}if(t.fixMinHeight){r.data("autogrow-start-height",i)}r.css("height",i);if(t.onInitialize&&r.length){s.call(r[0])}});t.context.on("keyup paste",r,s);return n}})(jQuery);
+$( document ).ready(function() {
+
+  $('textarea').autogrow({onInitialize: true});
+
+
+  //Cloner for infinite input lists
+  $(".circle--clone--list").on("click", ".circle--clone--add", function(){
+    var parent = $(this).parent("li");
+    var copy = parent.clone();
+    parent.after(copy);
+    copy.find("input, textarea, select").val("");
+    copy.find("*:first-child").focus();
+  });
+
+  $(".circle--clone--list").on("click", "li:not(:only-child) .circle--clone--remove", function(){
+    var parent = $(this).parent("li");
+    parent.remove();
+  });
+
+  // Adds class to selected item
+  $(".circle--pill--list a").click(function() {
+    $(".circle--pill--list a").removeClass("selected");
+    $(this).addClass("selected");
+  });
+
+  // Adds class to parent div of select menu
+  $(".circle--select select").focus(function(){
+   $(this).parent().addClass("focus");
+   }).blur(function(){
+     $(this).parent().removeClass("focus");
+   });
+
+  // Clickable table row
+  $(".clickable-row").click(function() {
+      var link = $(this).data("href");
+      var target = $(this).data("target");
+
+      if ($(this).attr("data-target")) {
+        window.open(link, target);
+      }
+      else {
+        window.open(link, "_self");
+      }
+  });
+
+  // Custom File Inputs
+  var input = $(".circle--input--file");
+  var text = input.data("text");
+  var state = input.data("state");
+  input.wrap(function() {
+    return "<a class='button " + state + "'>" + text + "</div>";
+  });
+
+
+
+
+});
